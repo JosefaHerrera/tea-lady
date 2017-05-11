3791fdd0bd11d9e5e41f94197568af41c9a300ff
JosefaHerrera <josefa@linuxero.cl>
2017-05-11 19:56:11 -0300

    arreglando HTML y hacer funcion constructor

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-function validarnombre() {
-	   if (document.form.name.value.length == 0){/*toma el largo del nombre, si es igual a 0 tira la alerta*/
+/*function validarnombre() {
+	   if (document.form.name.value.length == 0){
           alert("Debe ingresar su Nombre");
         
         } else {
             nombre = document.getElementById("name").value;
         }   
 }
+*/
+//objeto Constructo
