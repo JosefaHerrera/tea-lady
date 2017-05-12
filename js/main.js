24addc83b2089758f8d11938ab721298b66b72c0
JosefaHerrera <josefa@linuxero.cl>
2017-05-11 21:12:55 -0300

    contructor hecho y id

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@
 }
 */
 //objeto Constructo
+function Datos(nombre,apellido,correo,direccion,telefono){
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.correo = correo;
+	this.direccion = direccion;
+	this.telefono = telefono;
+
+};
+
+function mostrarData(){
+	var nombre =document. getElementById("name");
+	var apellido =document. getElementById("surname");
+	var correo =document. getElementById("correito");
+	var direccion =document. getElementById("adress");
+	var telefono =document. getElementById("phone");
+
+};
